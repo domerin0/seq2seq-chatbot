1b2103a82613067e4abfe546e929e1b05977607f
Dominik <domkaukinen@gmail.com>
2015-11-16 02:01:14 -0500

    cleaned up source-target pairing

--- Util/MiniBatchLoader.lua
+++ Util/MiniBatchLoader.lua
@@ -3,11 +3,14 @@ The purpose of this file is to enable the train.lua file to
 'efficiently' create, load, and save the minibatches
 ]]
 local TableUtils = require "Util.TableUtils"
+local Preprocessor = require "Util.Preprocessor"
+require 'lfs'
 local MiniBatchLoader = {}
 math.randomseed( os.time() )
 
-function MiniBatchLoader.createMiniBatches(dataFiles, batchSize, trainFrac,
-  evalFrac, testFrac)
+function MiniBatchLoader.createMiniBatches(dataDir, batchSize, trainFrac,
+  evalFrac, testFrac, maxSequenceLength)
+
   --Checks to ensure user isn't testing us
   assert(evalFrac >= 0 and evalFrac < 1, "evalFrac not between 0 and 1...")
   assert(trainFrac > 0 and trainFrac <= 1, "trainFrac not between 0 and 1...")
@@ -17,46 +20,28 @@ function MiniBatchLoader.createMiniBatches(dataFiles, batchSize, trainFrac,
   print("Using ".. trainFrac .. " As percentage of data to train on...")
   print("Using ".. evalFrac .. " As percentage of data to validate on...")
   print("Using " .. testFrac .. " As percentage of data to test on...")
-
+  print("Max sequence length is ... ".. maxSequenceLength)
   local batchFiles  = {}
   local trainFiles = {}
   local testFiles = {}
   local evalFiles = {}
-
+  local dataFiles = {}
+  local processedDir = path.join(dataDir, "processed/")
   print("Loading data...")
 
   local trainingPairSize = {}
 
+  for file in lfs.dir(processedDir) do
+    table.insert(dataFiles, path.join(processedDir, file))
+  end
+
+  local miniBatches = {}
+
   for key, value in pairs(dataFiles) do
-    --Due to memory constraints of data I have decided to split everything
-    --into multiple files
-    local batchFile = path.join(dataDir, "rawbatches/batch"..key..".t7")
-    table.insert(batchFiles, batchFile)
-
-    local trainFile = path.join(dataDir, "train/train.t7")
-    table.insert(trainFiles, trainFile)
-
-    local testFile = path.join(dataDir, "test/test.t7"))
-    table.insert(testFiles, testFile)
-
-    local evalFile = path.join(dataDir, "eval/eval.t7")
-    table.insert(evalFiles, evalFile)
-
     local data = torch.load(value)
-    print(#data)
-    --Sort sequences by length!
-    print("Finding longest sequence length...")
-    table.sort(data,
-      function (a,b)
-        return (#a > #b)
-      end
-      )
+    print("Loaded data...")
 
-    local maxSequenceLength = #data[1]
-    --reload values since order of them does matter for training purposes
-    data = torch.load(value)
-    print("Length of longest sequence is " .. maxSequenceLength .. "...")
-    local sourceTargetPairs = {}
+    local sourceTargetPairs = torch.Tensor(#data-1, 2, maxSequenceLength)
 
     -- Reverse sequences to introduce short-term dependency's (Google's result)
     --Insert training pairs into table
@@ -65,35 +50,40 @@ function MiniBatchLoader.createMiniBatches(dataFiles, batchSize, trainFrac,
       maxSequenceLength)
       data[i] = nil
       local target = TableUtils.padTable(data[i + 1], maxSequenceLength)
-      table.insert(sourceTargetPairs, {source, target})
-    end
-    local miniBatches = {}
-    local numBatches = math.floor(#sourceTargetPairs / batchSize)
-    local counter = 0
-    for i=1,#numBatches do
-      local batch = {}
-      for j=1,batchSize do
-        counter = counter + 1
-        local torchifiedBatch = sourceTargetPairs[counter]
-        table.insert(batch, torchifiedBatch)
-      end
-      table.insert(miniBatches, torch.Tensor(batch))
+      sourceTargetPairs[i] = torch.Tensor({source, target})
     end
 
-      --shuffle the minibatches in place
-    TableUtils.shuffleTable(miniBatches)
-    table.insert(trainingPairSize, #miniBatches)
-    torch.save(batchFile, miniBatches)
+
+    table.insert(miniBatches, sourceTargetPairs)
   end
-
+  print("Made it!")
       --create train/test/eval batch sets
-  local totalNum = TableUtils.sum(trainingPairSize)
-  local numTrain = math.floor(trainFrac * totalNum)
-  local numTest = math.floor(testFrac * totalNum)
-  local numEval = totalNum - numTrain - numTest
+
 
     --save batch sets in appropriate t7 files
-  for key, file in pairs(batchFiles)
+    print("Creating minibatch files...")
+  for key, file in pairs(batchFiles) do
+  --Due to memory constraints of data I have decided to split everything
+  --into multiple files
+
+    local batchFile = path.join(dataDir, "rawbatches/batch"..key..".t7")
+    table.insert(batchFiles, batchFile)
+
+    local trainFile = path.join(dataDir, "train/train"..key..".t7")
+    table.insert(trainFiles, trainFile)
+
+    local testFile = path.join(dataDir, "test/test"..key..".t7")
+    table.insert(testFiles, testFile)
+
+    local evalFile = path.join(dataDir, "eval/eval"..key..".t7")
+    table.insert(evalFiles, evalFile)
+
+    local miniBatches = torch.load(file)
+    local totalNum = #miniBatches
+
+    local numTrain = math.floor(trainFrac * totalNum)
+    local numTest = math.floor(testFrac * totalNum)
+    local numEval = totalNum - numTrain - numTest
     torch.save(batchFile, miniBatches)
     torch.save(trainFile, table.unpack(miniBatches, 1, numTrain))
     torch.save(testFile, table.unpack(miniBatches, numTrain + 1, numTrain + numTest))
--- Util/Preprocessor.lua
+++ Util/Preprocessor.lua
@@ -4,7 +4,6 @@ The main goal of this file is to run the preprocessor if necessary:
 2. Create minibatches save to tensor file
 ]]--
 require "lfs"
-local ffi = require('ffi')
 
 local Preprocessor = {}
 
@@ -13,14 +12,23 @@ local StringUtils = require "Util.StringUtils"
 --trainFrac is percentage of data to use for training
 -- validation data is computed as (1 - trainFrac)
 function Preprocessor.start(dataDir)
-
+  lfs.mkdir (dataDir)
   local vocabFile = path.join(dataDir, "vocab.t7")
-  local inputFile = path.join(dataDir, "raw.txt")
-  local processedDataDir  = path.join(dataDir, "processed/")
-  local dataFile1  = path.join(processedDataDir, "data1.t7")
-  local dataFile2  = path.join(processedDataDir, "data2.t7")
-
   local dicFile = path.join(dataDir, "index2Vec.t7")
+  local inputFile = path.join(dataDir, "raw.txt")
+  --Make directories we will need just incase they don't exist
+  local processedDataDir  = path.join(dataDir, "processed/")
+  local batchesDataDir = path.join(dataDir, "rawbatches/")
+  local trainDataDir = path.join(dataDir, "train/")
+  local testDataDir = path.join(dataDir, "test/")
+  local evalDataDir = path.join(dataDir, "eval/")
+  lfs.mkdir(batchesDataDir)
+  lfs.mkdir(trainDataDir)
+  lfs.mkdir(testDataDir)
+  lfs.mkdir(evalDataDir)
+  lfs.mkdir(processedDataDir)
+  local dataFiles = {}
+
 
   local runPreprocessor = false
 
@@ -44,35 +52,39 @@ function Preprocessor.start(dataDir)
     runPreprocessor = true
   end
 
-  if not path.exists(dataFile1) or not path.exists(dataFile2) then
-    print('data.t7 does not exist... Creating it...')
-    f=io.open(dataFile1,"w")
-    f:close()
-    f=io.open(dataFile2,"w")
-    f:close()
-  end
-
-  dataFiles = {dataFile1, dataFile2}
-
   local vocabAttributes = lfs.attributes(vocabFile)
+  local dicAttributes = lfs.attributes(dicFile)
   local inputAttributes = lfs.attributes(inputFile)
-  local tensorAttributes = lfs.attributes(dataFile)
   --[Iterate through all the text in file to tokenize it]
 
   if inputAttributes.modification > vocabAttributes.modification or
-    tensorAttributes.modification < inputAttributes.modification then
+    dicAttributes.modification < inputAttributes.modification then
       print("Vocab and or data files are old, re-processing...")
       runPreprocessor = true
   end
   print(runPreprocessor)
   local numLines = 0
+  local maxSequenceLength = 0
+
   if runPreprocessor then
     numLines = Preprocessor.createVocabFile(inputFile, vocabFile)
+
+    --1333139 was chosen because it represents about half
+    --the dataset
+    for i=1,math.floor(numLines/888759) do
+      local p = path.join(processedDataDir, "data"..i..".t7")
+      table.insert(dataFiles, p)
+      if not path.exists(p) then
+        f=io.open(p,"w")
+        f:close()
+      end
+    end
+
     Preprocessor.createDicFile(vocabFile, dicFile)
     collectgarbage()
-    Preprocessor.createDataFile(inputFile, vocabFile, dataFiles, numLines)
+    maxSequenceLength = Preprocessor.createDataFile(inputFile, vocabFile, dataFiles, numLines)
   end
-  return dataFiles
+  return maxSequenceLength
 end
 
 --[[ This function assumes that the raw.txt file has already been tokenized,
@@ -134,7 +146,7 @@ function Preprocessor.createDataFile(inputFile, vocabFile, dataFiles, numLines)
   local dataset = {}
   local lineCounter = 0
   local fileCounter = 1
-
+  local maxSequenceLength = 0
   for line in io.lines(inputFile) do
     lineCounter = lineCounter + 1
     local sequence = {}
@@ -148,15 +160,20 @@ function Preprocessor.createDataFile(inputFile, vocabFile, dataFiles, numLines)
     end
       --Must indicate end of sentence
       sequence[#sequence + 1] = vocabMapping["EOS"]
-      table.insert(dataset, torch.Tensor(sequence))
-      if fileCounter < #dataFiles and
-      lineCounter % math.floor(numLines / #dataFiles) == 0 then
+      if maxSequenceLength < #sequence then
+        maxSequenceLength = #sequence
+      end
+      table.insert(dataset, sequence)
+      --Use magic number determined above (or if < magic number use max lines)
+      if lineCounter % 888759 == 0 or lineCounter == numLines then
         torch.save(dataFiles[fileCounter], dataset)
         fileCounter = fileCounter  + 1
         dataset = {}
+        --the stop condition (we lose at most one data point doing this)
+        if fileCounter == #dataFiles + 1 then break end
       end
   end
-  torch.save(dataFile, dataset)
+  return maxSequenceLength
 end
 
 return Preprocessor
