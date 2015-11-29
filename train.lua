47d354c1deb6fadb20d3bdcf9a91dc75d368ae80
Dominik <domkaukinen@gmail.com>
2015-11-29 12:20:07 -0500

    aded seq2seq model, changes to LSTM cell

--- train.lua
+++ train.lua
@@ -2,6 +2,7 @@ local CommandLineArgs = require "Util.CommandLineArgs"
 local Preprocessor = require "Util.Preprocessor"
 local MiniBatchLoader = require "Util.MiniBatchLoader"
 local VerifyGPU = require "Util.VerifyGPU"
+local Seq2Seq = require "Models.Seq2Seq"
 
 local options = CommandLineArgs.trainCmdArgs()
 torch.manualSeed(options.seed)
@@ -27,10 +28,16 @@ end
 
 --Now, check and enable GPU usage:
 
---VerifyGPU.checkCuda(options.gpuid, options.seed)
+local cuid = VerifyGPU.checkCuda(options.gpuid, options.seed)
 
 
---VerifyGPU.checkOpenCl(options.gpuid, options.seed)
+local clid = VerifyGPU.checkOpenCl(options.gpuid, options.seed)
+
+if clid == -1 and cuid == -1 then
+  options.gpuid = -1
+end
+
+
 
 --Load minibatches into memory!
 
@@ -42,10 +49,54 @@ if not path.exists(options.checkpoints) then
   lfs.mkdir(options.checkpoints)
 end
 
+local fromCheckpoint = false
 if(string.len(options.startFrom) > 0) then
   print("Loading network parameters from checkpoint... "..options.startFrom)
   local checkpoint = torch.load(options.startFrom)
+  protos = checkpoint.protos
+  --Maybe check vocab here TODO?
+  print("rnnSize= "..checkpoint.options.rnnSize.." numLayers= "..checkpoint.options.numLayers..)
+  options.rnnSize = checkpoint.options.rnnSize
+  options.numLayers = checkpoint.options.numLayers
+  fromCheckpoint = true
+else
+  print('Creating a chatbot with ' .. options.num_layers .. ' layers')
+  protos = {}
+  local model = Seq2Seq.conversationalModel(options.embeddingSize,
+    options.vocabSize,options.rnnSize, options.numLayers, options.dropout)
+  protos.encoder = model.encoder
+  protos.decoder = model.decoder
+  protos.criterion = nn.CrossEntropyCriterion()
 end
+
+local initialState = {}
+for i=1,options.numLayers do
+  local hInit = torch.zeroes(options.batchSize, options.rnnSize)
+  if options.gpuid >=0 and options.opencl == 0 then hInit = hInit:cuda() end
+  if options.gpuid >=0 and options.opencl == 1 then hInit = hInit:cl() end
+  table.insert(initialState, initialState:clone())
+  table.insert(initialState, initialState:clone())
+end
+
+if options.gpuid >= 0 and options.opencl == 0 then
+  if options.opencl == 0 then
+    for k,v in pairs(protos) do v:cuda() end
+  end
+  if options.opencl == 1 then
+    for k,v in pairs(protos) do v:cl() end
+  end
+end
+
+encoderParams, encoderGradParams = model_utils.combine_all_parameters(protos.encoder)
+decoderParams, decoderGradParams = model_utils.combine_all_parameters(protos.decoder)
+
+if fromCheckpoint == false then
+  encoderParams:uniform(-0.08, 0.08)
+  decoderParams:uniform(-0.08, 0.08)
+end
+
+
+
 
 
 --perform training of n minibatches of m epochs over bs backsteps
